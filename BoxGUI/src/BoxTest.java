7750e9c027303278212f54fbe23e5ee86f7aeae1
Asfandyar Tanwer <91708459+Asfand00@users.noreply.github.com>
2023-11-19 05:33:42 -0800

    Create BoxTest.java

---
+++ BoxGUI/src/BoxTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * This is the Junit 5 test class for the box class
+ * Tests all methods and constructor is working correctly
+ *
+ * @author Asfandyar Tanwer
+ * @version 1.0
+ */
+class BoxTest {
+
+    @org.junit.jupiter.api.Test
+    void getLength() {
+        Box testBox = new Box(5,6,7);
+        Assertions.assertEquals(5,testBox.getLength());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWidth() {
+        Box testBox = new Box(5,6,7);
+        Assertions.assertEquals(6, testBox.getWidth());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHeight() {
+        Box testBox = new Box(5,6,7);
+        Assertions.assertEquals(7, testBox.getHeight());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLength() {
+        Box testBox = new Box(5,6,7);
+        testBox.setLength(-8);
+        Assertions.assertEquals(-8, testBox.getLength());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setWidth() {
+        Box testBox = new Box(5,6,7);
+        testBox.setWidth(9.45);
+        Assertions.assertEquals(9.45, testBox.getWidth());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setHeight() {
+        Box testBox = new Box(5,6,7);
+        testBox.setHeight(570.890);
+        Assertions.assertEquals(570.890, testBox.getHeight());
+    }
+}
